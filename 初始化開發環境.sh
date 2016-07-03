875f9442486f96fe7ed8a06772c95d4796433768
Ｖ字龍 <Vdragon.Taiwan@gmail.com>
2016-07-04 07:07:28 +0800

    初始化開發環境.sh - 讓程式不管在哪個工作目錄下都能正常運作
    Signed-off-by: Ｖ字龍 <Vdragon.Taiwan@gmail.com>

--- 初始化開發環境.sh
+++ 初始化開發環境.sh
@@ -17,6 +17,8 @@ readonly PROGRAM_DIRECTORY="$(realpath --strip "$(dirname "$0")")"
 readonly PROGRAM_ARGUMENT_ORIGINAL_LIST="$@"
 readonly PROGRAM_ARGUMENT_ORIGINAL_NUMBER=$#
 
+declare -r PROJECT_ROOT_DIRECTORY="$PROGRAM_DIRECTORY"
+
 ######## File scope variable definitions ended ########
 
 ######## Program ########
@@ -26,10 +28,10 @@ main() {
 	# 安裝專案專用 Git 設定
 	# git - Is it possible to include a file in your .gitconfig - Stack Overflow
 	# http://stackoverflow.com/questions/1557183/is-it-possible-to-include-a-file-in-your-gitconfig
-	git config --local include.path '../.gitconfig'
+	git --git-dir="$PROJECT_ROOT_DIRECTORY"/.git --work-tree="$PROJECT_ROOT_DIRECTORY" config --local include.path '../.gitconfig'
 	
 	# 安裝版本提交前 Git 掛勾程式
-	ln --symbolic --force "../../開發工具/Git 版本控制系統掛勾程式/pre-commit.sh" .git/hooks/pre-commit
+	ln --symbolic --force "../../開發工具/Git 版本控制系統掛勾程式/pre-commit.sh" "$PROJECT_ROOT_DIRECTORY"/.git/hooks/pre-commit
 	
 	## 正常結束 script 程式
 	exit 0
