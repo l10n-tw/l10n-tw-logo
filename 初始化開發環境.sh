48060e62d67adcf13649c9eb50a1f455fd62f53c
Ｖ字龍 <Vdragon.Taiwan@gmail.com>
2017-06-16 10:14:24 +0800

    初始化開發環境.sh - Use GNU Bash Shell Script Template
    Signed-off-by: Ｖ字龍 <Vdragon.Taiwan@gmail.com>

--- 初始化開發環境.sh
+++ 初始化開發環境.sh
@@ -1,30 +1,47 @@
 #!/usr/bin/env bash
-# 上列為宣告執行 script 程式用的殼程式(shell)的 shebang
-# 初始化開發環境.sh - 設定好要提交本專案程式碼時必須要有的開發環境
-# 林博仁 © 2016
-# 如題
+# shellcheck disable=SC2034
+# 設定好要提交本專案程式碼時必須要有的開發環境
+# Ｖ字龍 <Vdragon.Taiwan@gmail.com> © 2017
 
-######## Included files ########
+## Makes debuggers' life easier - Unofficial Bash Strict Mode
+## BASHDOC: Shell Builtin Commands - Modifying Shell Behavior - The Set Builtin
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
 
-######## Included files ended ########
+## Non-overridable Primitive Variables
+## BASHDOC: Shell Variables » Bash Variables
+## BASHDOC: Basic Shell Features » Shell Parameters » Special Parameters
+if [ -v "BASH_SOURCE[0]" ]; then
+	RUNTIME_EXECUTABLE_PATH="$(realpath --strip "${BASH_SOURCE[0]}")"
+	RUNTIME_EXECUTABLE_FILENAME="$(basename "${RUNTIME_EXECUTABLE_PATH}")"
+	RUNTIME_EXECUTABLE_NAME="${RUNTIME_EXECUTABLE_FILENAME%.*}"
+	RUNTIME_EXECUTABLE_DIRECTORY="$(dirname "${RUNTIME_EXECUTABLE_PATH}")"
+	RUNTIME_COMMANDLINE_BASECOMMAND="${0}"
+	declare -r\
+		RUNTIME_EXECUTABLE_FILENAME\
+		RUNTIME_EXECUTABLE_DIRECTORY\
+		RUNTIME_EXECUTABLE_PATHABSOLUTE\
+		RUNTIME_COMMANDLINE_BASECOMMAND
+fi
+declare -ar RUNTIME_COMMANDLINE_PARAMETERS=("${@}")
 
-######## File scope variable definitions ########
-# Defensive Bash Programming - not-overridable primitive definitions
-# http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
-readonly PROGRAM_FILENAME="$(basename "$0")"
-## 考慮向前相容 Ubuntu 14.04 LTS 將 --no-symlinks 替換為 --strip
-readonly PROGRAM_DIRECTORY="$(realpath --strip "$(dirname "$0")")"
-readonly PROGRAM_ARGUMENT_ORIGINAL_LIST="$@"
-readonly PROGRAM_ARGUMENT_ORIGINAL_NUMBER=$#
+## init function: entrypoint of main program
+## This function is called near the end of the file,
+## with the script's command-line parameters as arguments
+init(){
+	declare -r PROJECT_ROOT_DIRECTORY="${RUNTIME_EXECUTABLE_DIRECTORY}"
 
-declare -r PROJECT_ROOT_DIRECTORY="${PROGRAM_DIRECTORY}"
+	if ! process_commandline_parameters; then
+		printf\
+			"Error: %s: Invalid command-line parameters.\n"\
+			"${FUNCNAME[0]}"\
+			1>&2
+		print_help
+		exit 1
+	fi
 
-######## File scope variable definitions ended ########
-
-######## Program ########
-# Defensive Bash Programming - main function, program entry point
-# http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
-main() {
 	export\
 		GIT_DIR="${PROJECT_ROOT_DIRECTORY}"/.git\
 		GIT_WORK_TREE="${PROJECT_ROOT_DIRECTORY}"
@@ -37,8 +54,85 @@ main() {
 	# 安裝版本提交前 Git 掛勾程式
 	ln --symbolic --force "../../開發工具/Git 版本控制系統掛勾程式/pre-commit.sh" "${PROJECT_ROOT_DIRECTORY}"/.git/hooks/pre-commit
 	
-	## 正常結束 script 程式
 	exit 0
-}
-main
-######## Program ended ########
+}; declare -fr init
+
+## Traps: Functions that are triggered when certain condition occurred
+## Shell Builtin Commands » Bourne Shell Builtins » trap
+trap_errexit(){
+	printf "An error occurred and the script is prematurely aborted\n" 1>&2
+	return 0
+}; declare -fr trap_errexit; trap trap_errexit ERR
+
+trap_exit(){
+	return 0
+}; declare -fr trap_exit; trap trap_exit EXIT
+
+trap_return(){
+	local returning_function="${1}"
+
+	printf "DEBUG: %s: returning from %s\n" "${FUNCNAME[0]}" "${returning_function}" 1>&2
+}; declare -fr trap_return
+
+trap_interrupt(){
+	printf "Recieved SIGINT, script is interrupted.\n" 1>&2
+	return 0
+}; declare -fr trap_interrupt; trap trap_interrupt INT
+
+print_help(){
+	printf "Currently no help messages are available for this program\n" 1>&2
+	return 0
+}; declare -fr print_help;
+
+process_commandline_parameters() {
+	if [ "${#RUNTIME_COMMANDLINE_PARAMETERS[@]}" -eq 0 ]; then
+		return 0
+	fi
+
+	# modifyable parameters for parsing by consuming
+	local -a parameters=("${RUNTIME_COMMANDLINE_PARAMETERS[@]}")
+
+	# Normally we won't want debug traces to appear during parameter parsing, so we  add this flag and defer it activation till returning(Y: Do debug)
+	local enable_debug=N
+
+	while true; do
+		if [ "${#parameters[@]}" -eq 0 ]; then
+			break
+		else
+			case "${parameters[0]}" in
+				"--help"\
+				|"-h")
+					print_help;
+					exit 0
+					;;
+				"--debug"\
+				|"-d")
+					enable_debug="Y"
+					;;
+				*)
+					printf "ERROR: Unknown command-line argument \"%s\"\n" "${parameters[0]}" >&2
+					return 1
+					;;
+			esac
+			# shift array by 1 = unset 1st then repack
+			unset "parameters[0]"
+			if [ "${#parameters[@]}" -ne 0 ]; then
+				parameters=("${parameters[@]}")
+			fi
+		fi
+	done
+
+	if [ "${enable_debug}" = "Y" ]; then
+		trap 'trap_return "${FUNCNAME[0]}"' RETURN
+		set -o xtrace
+	fi
+	return 0
+}; declare -fr process_commandline_parameters;
+
+init "${@}"
+
+## This script is based on the GNU Bash Shell Script Template project
+## https://github.com/Lin-Buo-Ren/GNU-Bash-Shell-Script-Template
+## and is based on the following version:
+declare -r META_BASED_ON_GNU_BASH_SHELL_SCRIPT_TEMPLATE_VERSION="v1.26.0-32-g317af27-dirty"
+## You may rebase your script to incorporate new features and fixes from the template
