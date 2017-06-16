d175a5a8313412e6938e1b6573a190069dada761
Ｖ字龍 <Vdragon.Taiwan@gmail.com>
2017-06-16 10:06:20 +0800

    Coding Style: Bash: wrap curly braces to parameter names
    Signed-off-by: Ｖ字龍 <Vdragon.Taiwan@gmail.com>

--- 軟體建構解決方案/批次命令/build.china-communist.source.sh
+++ 軟體建構解決方案/批次命令/build.china-communist.source.sh
@@ -1,16 +1,16 @@
-	declare new_source_file="$DIRECTORY_BUILD_ARTIFACTS/${SOFTWARE_IDENTIFIER}-china-communist.svg"
+	declare new_source_file="${DIRECTORY_BUILD_ARTIFACTS}/${SOFTWARE_IDENTIFIER}-china-communist.svg"
 	printf "資訊：正在建構中華人民共和國共產黨版……\n"
-	cp "${FILE_SOURCE_DESIGN}" "$new_source_file"
-	xmlstarlet edit --pf --ps --inplace --update "//_:g[@inkscape:label='官方版本（LION 版v2）']/@style" --value "display:none" "$new_source_file"
-	xmlstarlet edit --pf --ps --inplace --update "//_:g[@inkscape:label='官方版本（品牌名）']/@style" --value "display:none" "$new_source_file"
-	xmlstarlet edit --pf --ps --inplace --update "//_:g[@inkscape:label='中華人民共和國共產黨版']/@style" --value "display:inline" "$new_source_file"
-	inkscape --export-png="$DIRECTORY_BUILD_ARTIFACTS/$(basename --suffix=.svg "$new_source_file").png" "$new_source_file"
+	cp "${FILE_SOURCE_DESIGN}" "${new_source_file}"
+	xmlstarlet edit --pf --ps --inplace --update "//_:g[@inkscape:label='官方版本（LION 版v2）']/@style" --value "display:none" "${new_source_file}"
+	xmlstarlet edit --pf --ps --inplace --update "//_:g[@inkscape:label='官方版本（品牌名）']/@style" --value "display:none" "${new_source_file}"
+	xmlstarlet edit --pf --ps --inplace --update "//_:g[@inkscape:label='中華人民共和國共產黨版']/@style" --value "display:inline" "${new_source_file}"
+	inkscape --export-png="${DIRECTORY_BUILD_ARTIFACTS}/$(basename --suffix=.svg "${new_source_file}").png" "${new_source_file}"
 	
-	declare new_source_file_white_background="$DIRECTORY_BUILD_ARTIFACTS/${SOFTWARE_IDENTIFIER}-china-communist-background-white.svg"
+	declare new_source_file_white_background="${DIRECTORY_BUILD_ARTIFACTS}/${SOFTWARE_IDENTIFIER}-china-communist-background-white.svg"
 	printf "資訊：正在建構中華人民共和國共產黨版白背景版……\n"
-	cp "$new_source_file" "$new_source_file_white_background"
-	xmlstarlet edit --pf --ps --inplace --update "//@inkscape:pageopacity" --value 1 "$new_source_file_white_background"
-	inkscape --export-png="$DIRECTORY_BUILD_ARTIFACTS/$(basename --suffix=.svg "$new_source_file_white_background").png" "$new_source_file_white_background"
+	cp "${new_source_file}" "${new_source_file_white_background}"
+	xmlstarlet edit --pf --ps --inplace --update "//@inkscape:pageopacity" --value 1 "${new_source_file_white_background}"
+	inkscape --export-png="${DIRECTORY_BUILD_ARTIFACTS}/$(basename --suffix=.svg "${new_source_file_white_background}").png" "${new_source_file_white_background}"
 	
 	unset new_source_file
 	unset new_source_file_white_background
--- 軟體建構解決方案/批次命令/build.lion.source.sh
+++ 軟體建構解決方案/批次命令/build.lion.source.sh
@@ -1,15 +1,15 @@
-	declare new_source_file="$DIRECTORY_BUILD_ARTIFACTS/${SOFTWARE_IDENTIFIER}-lion.svg"
+	declare new_source_file="${DIRECTORY_BUILD_ARTIFACTS}/${SOFTWARE_IDENTIFIER}-lion.svg"
 	printf "資訊：正在建構 LION 版……\n"
-	cp "${FILE_SOURCE_DESIGN}" "$new_source_file"
-	xmlstarlet edit --pf --ps --inplace --update "//_:g[@inkscape:label='官方版本（LION 版v2）']/@style" --value "display:none" "$new_source_file"
-	xmlstarlet edit --pf --ps --inplace --update "//_:g[@inkscape:label='官方版本（LION 版）']/@style" --value "display:inline" "$new_source_file"
-	inkscape --export-png="$DIRECTORY_BUILD_ARTIFACTS/$(basename --suffix=.svg "$new_source_file").png" "$new_source_file"
+	cp "${FILE_SOURCE_DESIGN}" "${new_source_file}"
+	xmlstarlet edit --pf --ps --inplace --update "//_:g[@inkscape:label='官方版本（LION 版v2）']/@style" --value "display:none" "${new_source_file}"
+	xmlstarlet edit --pf --ps --inplace --update "//_:g[@inkscape:label='官方版本（LION 版）']/@style" --value "display:inline" "${new_source_file}"
+	inkscape --export-png="${DIRECTORY_BUILD_ARTIFACTS}/$(basename --suffix=.svg "${new_source_file}").png" "${new_source_file}"
 	
-	declare new_source_file_white_background="$DIRECTORY_BUILD_ARTIFACTS/${SOFTWARE_IDENTIFIER}-lion-background-white.svg"
+	declare new_source_file_white_background="${DIRECTORY_BUILD_ARTIFACTS}/${SOFTWARE_IDENTIFIER}-lion-background-white.svg"
 	printf "資訊：正在建構 LION 版白背景版……\n"
-	cp "$new_source_file" "$new_source_file_white_background"
-	xmlstarlet edit --pf --ps --inplace --update "//@inkscape:pageopacity" --value 1 "$new_source_file_white_background"
-	inkscape --export-png="$DIRECTORY_BUILD_ARTIFACTS/$(basename --suffix=.svg "$new_source_file_white_background").png" "$new_source_file_white_background"
+	cp "${new_source_file}" "${new_source_file_white_background}"
+	xmlstarlet edit --pf --ps --inplace --update "//@inkscape:pageopacity" --value 1 "${new_source_file_white_background}"
+	inkscape --export-png="${DIRECTORY_BUILD_ARTIFACTS}/$(basename --suffix=.svg "${new_source_file_white_background}").png" "${new_source_file_white_background}"
 	
 	unset new_source_file
 	unset new_source_file_white_background
