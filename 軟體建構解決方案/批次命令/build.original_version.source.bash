f253bf037cb29c5045012e69665d1265b8bbae43
Ｖ字龍 <Vdragon.Taiwan@gmail.com>
2017-06-21 13:03:05 +0800

    建構文字轉路徑版
    Signed-off-by: Ｖ字龍 <Vdragon.Taiwan@gmail.com>

--- 軟體建構解決方案/批次命令/build.original_version.source.bash
+++ 軟體建構解決方案/批次命令/build.original_version.source.bash
@@ -3,6 +3,7 @@
 		target_file
 
 	target_file="${DIRECTORY_BUILD_ARTIFACTS}/$(basename "${FILE_SOURCE_DESIGN}")"
+	target_file_text_to_path="$(basename --suffix .svg "${target_file}")-text-to-path.svg"
 
 	printf --\
 		"資訊：正在建構「%s」版……\n"\
@@ -18,6 +19,20 @@
 		"${variant_title}"\
 		"${target_file}"
 
+	cp\
+		"${FILE_SOURCE_DESIGN}"\
+		"${target_file_text_to_path}"
+
+	inkscape\
+		--without-gui\
+		--select=brand-zh-tw-original-tai\
+		--select=brand-china-communist\
+		--select=official-site-domain\
+		--select=official-site-domain-capitalized\
+		--verb=ObjectToPath\
+		"${target_file_text_to_path}"
+
 	unset\
 		variant_title\
-		target_file
+		target_file\
+		target_file_text_to_path
